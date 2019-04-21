ead81c41e072da2c33adfca722794cc7dcb9caaf
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-04-20 23:34:20 -0400

    installed socket client and serverside

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var path = require('path');
+var io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function(req, res){
+
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
+io.on('connection', function(socket) {
+  console.log('a user connected');
+});
+
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
