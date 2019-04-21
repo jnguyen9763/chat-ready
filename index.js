46033b9ac8bc3a0a24e70a7b4099dec62836e1f0
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-04-20 23:15:50 -0400

    created pubilc folder for static content

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
+var path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
