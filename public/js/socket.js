e113bfd7901b8cd35397ba907a864a6faaee6928
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-04-20 23:50:01 -0400

    completed tutorial, chat works

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,1 +1,14 @@
-var socket = io();
+$(function () {
+  var socket = io();
+
+  $('form').submit(function(e) {
+    e.preventDefault(); // prevents page reoloading
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+
+  socket.on('chat message', function(msg) {
+    $('#messages').append($('<li>').text(msg));
+  });
+});
